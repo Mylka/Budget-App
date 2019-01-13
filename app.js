bb86b07dde7af88d7b7229aadd022aa4695bf4b1
Mary Em <lmary155@gmail.com>
2019-01-13 22:06:07 +0100

    update the UI budget controller

--- app.js
+++ app.js
@@ -160,6 +160,11 @@ var UIController = (function(){
 
         },
 
+        deleteListItem: function(selectorID){
+            var el = document.getElementById(selectorID);
+            el.parentNode.removeChild(el);
+        },
+
         clearFields: function(){
             var fields, fieldsArray;
             fields = document.querySelectorAll(DOMstrings.inputDescription + ' ,' + DOMstrings.inputValue);
@@ -256,8 +261,11 @@ var controller = (function(budgetCtrl, UICtrl){
             budgetCtrl.deleteItem(type,ID);
 
             //2. delete the item from the UI
+            UICtrl.deleteListItem(itemID);
 
             //3. update and show the new budget
+            updateBudget();
+
         }
     };
 
